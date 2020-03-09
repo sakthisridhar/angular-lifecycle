58a81966ead4b3e4c9ee2a1865fcd71b8648aea8
sakthisridhar <dsakthisridhar@gmail.com>
2020-03-09 13:07:04 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements AfterViewInit {
   empname='Employee Name';
+  @ViewChild('inputcls',{static:true})inputcls:ElementRef;
+
+  inputstr:string='Mobius Knowledge Services';
+  constructor(){
+    console.log(this.inputstr.match(/[aeiou]/g));
+  }
+
+  ngAfterViewInit(){
+    this.inputcls.nativeElement.class = 'ssss';
+    console.log();
+  }
   fromchild(event){
     this.empname=event;
   }
